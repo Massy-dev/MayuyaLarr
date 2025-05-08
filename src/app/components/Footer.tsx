9833675b81df803fb171405ac6649a3f8fda9f6b
Massy-dev <massy1833.dev@gmail.com>
2025-05-08 19:42:58 +0000

    Dernière mis à jour avec logo

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Crown, Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
-import Image from 'next/image';
+import {  Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
+
 
 
 
